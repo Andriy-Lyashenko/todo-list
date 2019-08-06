ae60132d9298eb93e8ccca64eb60b57a8c3d59b1
Andriy Lyashenko <andriy.lyashenko@valtech.com>
2019-08-07 01:39:08 +0300

    Fix bugs

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -9,7 +9,7 @@ const TodoItem = ({item,setActive,remItem}) => {
             <p onClick={() => setActive(item.id)}>
               {item.desc}
               {item.active ? <div className="active_item"></div> : ''}
-              <span className="quant_item">{item.comments.length}</span>
+              {item.comments.length > 0 ?<span className="quant_item">{item.comments.length}</span> : ''}
             </p>
             <button onClick={()=> remItem(item)}>Delete</button>
        </div>
