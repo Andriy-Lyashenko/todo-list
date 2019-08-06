07ff52e3244142da2f57514b97804e208b0308a5
Andriy Lyashenko <andriy.lyashenko@valtech.com>
2019-08-07 01:35:39 +0300

    Updates

---
+++ precache-manifest.af4d416561fe5fb6b8e0f14edc7e121b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cdb3b4223c121266d4b011d108a1d75",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "910731c3f619628e26e4",
+    "url": "/todo-list/static/css/main.874b990c.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/todo-list/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "910731c3f619628e26e4",
+    "url": "/todo-list/static/js/main.ac0d6373.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
